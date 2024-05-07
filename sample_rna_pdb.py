a1f9156ba947a9d84fb81c8416bd2f8dba178b8f
Marek Justyna <mjustyna@cs.put.poznan.pl>
2024-05-07 02:54:34 +0000

    Seed sampling as well

--- sample_rna_pdb.py
+++ sample_rna_pdb.py
@@ -6,6 +6,18 @@ from datasets import RNAPDBDataset
 from utils import Sampler, SampleToPDB
 from main_rna_pdb import sample
 
+from torch_geometric import seed_everything
+import random
+import numpy as np
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    seed_everything(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=0, help='GPU number.')
@@ -16,16 +28,17 @@ def main():
     parser.add_argument('--n_layer', type=int, default=2, help='Number of hidden layers.')
     parser.add_argument('--dim', type=int, default=64, help='Size of input hidden units.')
     parser.add_argument('--batch_size', type=int, default=8, help='batch_size')
-    parser.add_argument('--cutoff_l', type=float, default=0.52, help='cutoff in local layer')
-    parser.add_argument('--cutoff_g', type=float, default=2.00, help='cutoff in global layer')
-    parser.add_argument('--timesteps', type=int, default=500, help='timesteps')
+    parser.add_argument('--cutoff_l', type=float, default=0.2, help='cutoff in local layer')
+    parser.add_argument('--cutoff_g', type=float, default=0.5, help='cutoff in global layer')
+    parser.add_argument('--timesteps', type=int, default=1000, help='timesteps')
     parser.add_argument('--wandb', action='store_true', help='Use wandb for logging')
     parser.add_argument('--mode', type=str, default='coarse-grain', help='Mode of the dataset')
     args = parser.parse_args()
 
+    set_seed(args.seed)
     # Load the model
-    epoch = 20000
-    model_path = f"save/model_{epoch}.h5"
+    epoch = 150
+    model_path = f"save/cool-bee-109/model_{epoch}.h5"
     config = Config(dataset=args.dataset, dim=args.dim, n_layer=args.n_layer, cutoff_l=args.cutoff_l, cutoff_g=args.cutoff_g, mode=args.mode)
     model = PAMNet(config)
     model.load_state_dict(torch.load(model_path))
@@ -34,9 +47,9 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("Device: ", device)
     model.to(device)
-    ds = RNAPDBDataset("data/RNA-PDB/", name='train-raw-pkl', mode='coarse-grain')
-    ds_loader = DataLoader(ds, batch_size=4, shuffle=False)
-    sampler = Sampler(timesteps=500)
+    ds = RNAPDBDataset("data/RNA-PDB/", name='test-pkl', mode='coarse-grain')
+    ds_loader = DataLoader(ds, batch_size=3, shuffle=False)
+    sampler = Sampler(timesteps=args.timesteps)
     print("Sampling...")
     sample(model, ds_loader, device, sampler, epoch, num_batches=1)
 
