8f968f28cb34f54034879ba8c9fcae8ed83c27e2
Marek Justyna <mjustyna@cs.put.poznan.pl>
2024-05-23 03:21:02 +0000

    Max batch_size is 256

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -i
 #SBATCH -p hgx
-#SBATCH --gres=gpu:08
+#SBATCH --gres=gpu:04
 #SBATCH -t 168:00:00
 
 source ~/.bashrc
@@ -8,5 +8,5 @@ conda activate gnn
 
 torchrun \
     --standalone \
-    --nproc_per_node=8 \
+    --nproc_per_node=4 \
     main_rna_pdb.py --dataset RNA-PDB --epoch=401 --batch_size=64 --dim=256 --n_layer=8 --lr=1e-3 --timesteps=5000 --mode=coarse-grain --knn=20 --wandb
